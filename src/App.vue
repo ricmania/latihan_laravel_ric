0c542b1416c758369c2fcc618e7d972e33f7617e
SOULMATE-PLUS\SEKOLAH-13 <rickosyahputra2006@gmail.com>
2026-09-16 23:35:40 +0700

    chore: initialize project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const apiStatus = ref("Menghubungkan ke API...");
+
+onMounted(async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/status");
+    const data = await response.json();
+    apiStatus.value = data.message;
+  } catch (error) {
+    apiStatus.value = "API tidak dapat diakses";
+  }
+});
+</script>
+
+<template>
+  <main>
+    <h1>Pemrograman Web 2</h1>
+    <h2>Status Backend</h2>
+    <p>{{ apiStatus }}</p>
+  </main>
+</template>
